2aced3b81a4cbd30dc7ece15406cc64c3c4873e9
gywn0343 <gywn0343@gmail.com>
2018-11-12 06:49:43 -0800

    check msg send -> not sending any info. Also nothing is read in sender, too.

--- receiver.c
+++ receiver.c
@@ -13,8 +13,72 @@
 #define PACKET_LOSS_PROB 0.02
 #define PORT 10080
 #define SOCK_BUF_SIZE 10000000
+#define FILE_NUM 10000
+#define INT_DIGITS 19
 
+typedef struct ACK{
+	char fileName[BUFLEN];
+	int ack;
+}ACK;
+ACK ack[FILE_NUM];
+typedef struct MSG{
+	char fileName[BUFLEN];
+	char buf[BUFLEN];
+	int seq;
+}MSG;
 
+char* itoa(int i)
+{
+	static char buf[INT_DIGITS + 2];
+	char* p = buf + INT_DIGITS + 1;
+	do
+	{
+		*--p = '0' + (i % 10);
+		i /= 10;
+	} while(i != 0);
+	return p;
+}
+int receive_file(char *fileName, char *buf, int seq)
+{
+	int i;
+	printf("%s:/n%s\nseq: %d", fileName, buf, seq);
+	for(i=0;i<FILE_NUM;i++)
+	{
+		if(strcmp(ack[i].fileName, fileName) == 0)
+		{
+			ack[i].ack;
+			if(ack[i].ack < seq) ack[i].ack = seq;
+			break;
+		}
+		if(ack[i].ack == 0) 
+		{
+			ack[i].ack = seq;
+			break;
+		}
+	}
+
+	int fp;
+	if(seq == 0)
+	{
+		if((fp = open(fileName, O_RDWR | O_CREAT, 0755)) == -1)
+		{
+			perror("file create");
+			exit(1);
+		}
+	}
+	else
+	{
+		if((fp = open(fileName, O_RDWR | O_APPEND)) == -1)
+		{
+			perror("file open");
+			exit(1);
+		}
+	}
+
+	write(fp, buf, sizeof(buf));
+	close(fp);
+	return ack[i].ack;
+}
 int main(void)
 {
 	struct sockaddr_in recv_addr, send_addr;
@@ -22,7 +86,7 @@ int main(void)
 	int recv_sock, i, slen = sizeof(send_addr);
 	char buf[BUFLEN];
 	int ret;
-	int sock_buf_size = -1;
+	int sock_buf_size;
 	int new_sock_buf_size = SOCK_BUF_SIZE;
 	socklen_t len;
 	int sock_buf_len = sizeof(sock_buf_size);
@@ -47,8 +111,9 @@ int main(void)
 		perror("setsockopt error(SO_REUSEADDR)");
 		exit(1);
 	}
-	if(sock_buf_size < 10000000)
+	if(sock_buf_size < SOCK_BUF_SIZE)
 	{
+	printf("%d\n", new_sock_buf_size);
 		if(setsockopt(recv_sock, SOL_SOCKET, SO_RCVBUF, &new_sock_buf_size, sizeof(new_sock_buf_size)) < 0)
 		{
 			perror("setsockopt error(SO_RCVBUF)");
@@ -72,20 +137,32 @@ int main(void)
 		perror("bind");
 		exit(1);
 	}
+	printf("bind success!\n");
+	MSG* msg = (MSG*)malloc(sizeof(MSG));
 	while(1)
 	{
-		//try to receive some data, this is a blocking call
-		if ((ret = recvfrom(recv_sock, buf, BUFLEN, 0, (struct sockaddr *) &send_addr, &slen)) == -1)
+		
+		if (recvfrom(recv_sock, buf, BUFLEN, 0, (struct sockaddr *) &send_addr, &slen) == -1)
+			continue;
+
+		printf("file Name: %s\n", buf);
+		break;
+	}
+	while(1)
+	{
+		if (recvfrom(recv_sock, msg, sizeof(struct MSG), 0, (struct sockaddr *) &send_addr, &slen) == -1)
 		{
 			continue;
 		}
-		printf("fileName: %s\n", buf);
-		//print details of the client/peer and the data received
+		ret = receive_file(msg->fileName, msg->buf, msg->seq);
+		char* s_ret = itoa(ret);
 
 		
 		//now reply the client with the same data
-		if (sendto(recv_sock, buf, ret, 0, (struct sockaddr*) &send_addr, slen) == -1)
+		if (sendto(recv_sock, s_ret, sizeof(s_ret), 0, (struct sockaddr*) &send_addr, slen) == -1)
 		{
+			perror("sendto");
+			exit(1);
 		}
 	}
 
--- sender.c
+++ sender.c
@@ -6,20 +6,132 @@
 #include <time.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <sys/wait.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 
 #define BUFLEN 1400	//Max length of buffer
+#define ACKLEN 21
+#define INF 999999999
 #define PORT 10080	//The port on which to send data
 
 int WIN;
 int TIME;
 
+typedef struct MSG{
+	char fileName[BUFLEN];
+	char buf[BUFLEN];
+	int seq;
+}MSG;
+
+void slide_window(int ack, MSG **msg, int* msg_cnt, int* p_msg)
+{
+	int i, j;
+	int cnt = 0;
+	MSG cur;
+	for(i=0;i<WIN;i++)
+	{
+		if((*msg)[i].seq <= ack)
+		{
+			cnt++;
+			for(j=i+1;j<WIN;j++)
+			{
+				(*msg)[j-1] = (*msg)[j];
+			}
+			(*msg)[WIN-1].seq = INF;
+		}
+	}
+	*msg_cnt -= cnt;
+	*p_msg = WIN - 1 - cnt;
+}
+int isOver(MSG* msg)
+{
+	return msg[0].seq == INF;
+}
+void do_fileSend(int send_sock, struct sockaddr_in recv_addr, int slen, char* fileName)
+{
+	int ret;
+	int i, fp;
+	MSG *msg = (struct MSG*)malloc(sizeof(struct MSG) * WIN);
+	char buf[BUFLEN + 1];
+	char ack[ACKLEN + 1];
+	int p_msg = 0;
+	int msg_cnt = 0;
+	int p_win = 0;
+	int seq = 0;
+	clock_t start = clock();
+	clock_t now;
+	if((fp = open(fileName, O_RDONLY)) == -1)
+	{
+		perror("File open");
+		exit(1);
+	}
+	int fd[2];
+	if(pipe(fd) == -1)
+	{
+		perror("pipe");
+		exit(1);
+	}
+	pid_t pid = fork();
+	if(pid < 0) 
+	{
+		perror("fork");
+		exit(1);
+	}
+	if(pid > 0) // parent
+	{
+		close(fd[1]);
+		MSG *cur_msg = (struct MSG*)malloc(sizeof(struct MSG));
+		while(1)
+		{
+			for(;msg_cnt < WIN;msg_cnt++)
+			{
+printf("%d %d\n", msg_cnt, WIN);
+				if(read(fp, buf, BUFLEN) == 0)
+				{
+					close(fp);
+					break;
+				}
+				cur_msg = &(msg[p_msg + msg_cnt]);
+				strcpy(cur_msg->fileName, fileName);
+				strcpy(cur_msg->buf, buf);
+				cur_msg->seq = seq++;
+printf("%s: %d\n", msg[p_msg + msg_cnt].fileName, msg[p_msg + msg_cnt].seq);
+printf("%s: %d\n", cur_msg->fileName, cur_msg->seq);
+				sendto(send_sock, cur_msg, sizeof(struct MSG), 0, (struct sockaddr*)&recv_addr, slen);
+			}	
+printf("window...\n");
+for(i=0;i<WIN;i++)
+{
+	printf("%s", msg[i].buf);
+}
+			read(fd[0], ack, ACKLEN);
+printf("ack in parent: %s\n", ack);
+			slide_window(atoi(ack), &msg, &msg_cnt, &p_msg);
+			if(isOver(msg))
+			{
+				close(fp);
+				return;
+			}
+		}
+	}
+	else if(pid == 0) // child
+	{
+		close(fd[0]);
+		while(1)
+		{
+			if(recvfrom(send_sock, ack, ACKLEN, 0, (struct sockaddr*)&recv_addr, &slen) == -1)
+				continue;
+			write(fd[1], ack, sizeof(ack));
+printf("ack in child: %s\n", ack);
+		}
+	}
+}
+
 int main(void)
 {
 	struct sockaddr_in recv_addr;
 	int send_sock, i, slen=sizeof(recv_addr);
-	char buf[BUFLEN];
 	char message[BUFLEN];
 	char IP[20], ret;
 
@@ -48,6 +160,7 @@ int main(void)
 	}
 
 	char* fileName = (char*)malloc(sizeof(char)*BUFLEN);
+	int pid;
 	while(1)
 	{
 		printf("file name: ");
@@ -59,17 +172,27 @@ int main(void)
 			exit(1);
 		}
 		
-		memset(buf,'\0', BUFLEN);
+		if((pid = fork()) < 0)
+		{
+			puts("fork error");
+			return -1;
+		}
+		if(pid == 0)  // child
+		{
+			do_fileSend(send_sock, recv_addr, slen, fileName);
+		}
+
+		
+		//memset(buf,'\0', BUFLEN);
 		//try to receive some data, this is a blocking call
-		if (recvfrom(send_sock, buf, BUFLEN, 0, (struct sockaddr*)&recv_addr, &slen) == -1)
+		/*if (recvfrom(send_sock, buf, BUFLEN, 0, (struct sockaddr*)&recv_addr, &slen) == -1)
 		{
 			perror("recvfrom error");
 			exit(1);
 		}
 		
-		puts(buf);
+		puts(buf);*/
 	}
-
 	close(send_sock);
 	return 0;
 }
